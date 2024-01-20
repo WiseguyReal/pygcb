331726a12cf159584f9fa00354548524e579ab82
WiseguyReal <114261885+WiseguyReal@users.noreply.github.com>
2024-01-20 17:27:49 -0500

    Update shapes.py
    Perimeter + Area of a Triangle Class

--- assignment01/shapes.py
+++ assignment01/shapes.py
@@ -22,5 +22,17 @@ def describe_shape(shape):
 
 # Your code below
 
-class Triangle(Shape):
-    pass
+class Triangle(l1, l2, l3):
+    def __init__(self, l1, l2, l3):
+        self.l1 = l1
+        self.l2 = l2
+        self.l3 = l3
+    def get_area(l1, l2, l3):
+        l4 = 0.5 * l2
+        return l4 * l1 * l3
+
+    def get_perimeter(l1, l2, l3):
+        return l1 + l2 + l3
+
+triangle = Triangle(2, 4, 6)
+print(triangle.get_area)
